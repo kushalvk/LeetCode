9f521ad1a8b0b7166613c4959c914921ca917b95
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2026-07-31 10:50:05 +0530

    LC-valid-triangle-number [Runtime: N/A; Memory: N/A]

--- sorting/valid-triangle-number.cpp
+++ sorting/valid-triangle-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         int count = 0;
         for(int i = 0; i < nums.size(); i++) {
             for(int j = i + 1; j < nums.size(); j++) {
