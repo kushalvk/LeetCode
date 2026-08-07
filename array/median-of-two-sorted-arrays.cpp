736bb1de6ff5cfb35cc340a123dcb0cc5e4c452c
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2026-08-07 23:39:36 +0530

    LC-median-of-two-sorted-arrays [Runtime: N/A; Memory: N/A]

--- array/median-of-two-sorted-arrays.cpp
+++ array/median-of-two-sorted-arrays.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> v;
+        vector<int> nums3(nums1.size() + nums2.size());
 
-        for (int num : nums1) {
-            v.push_back(num);
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+
+        merge(nums1.begin(),nums1.end(),nums2.begin(),nums2.end(),nums3.begin());
+
+        int n = nums1.size() + nums2.size();
+
+        if(n % 2 == 0){
+            return (nums3[n/2 - 1] + nums3[n/2])/2.0;
+        }else{
+            return nums3[n/2];
         }
 
-        for (int num : nums2) {
-            v.push_back(num);
-        }
-
-        sort(v.begin(), v.end());
-
-        int n = v.size();
-
-        return n % 2 ? v[n / 2] : (v[n / 2 - 1] + v[n / 2]) / 2.0;
     }
 };
