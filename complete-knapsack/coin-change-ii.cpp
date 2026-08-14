76c6305864138972a05a5c47da03470320597ebe
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2026-08-14 20:42:51 +0530

    LC-coin-change-ii [Runtime: N/A; Memory: N/A]

--- complete-knapsack/coin-change-ii.cpp
+++ complete-knapsack/coin-change-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        vector<int> dp(amount + 1, 0);
+        vector<long long> dp(amount + 1, 0);
 
         dp[0] = 1;
 
