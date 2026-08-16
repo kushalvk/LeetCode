2e3b691639451b78d46d4ee91c73d7184b3a00b5
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2026-08-16 23:29:08 +0530

    LC-missing-number [Runtime: N/A; Memory: N/A]

--- binary-search/missing-number.cpp
+++ binary-search/missing-number.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int actualSum = n * (n + 1) / 2;
-        int currentSum = 0;
+        int totalSum = n * (n + 1) / 2;
+        int currunt = 0;
 
         for(int i = 0; i < n; i++) {
-            currentSum += nums[i];
+            currunt += nums[i];
         }
 
-        return actualSum - currentSum;
+        return totalSum - currunt; 
     }
 };
