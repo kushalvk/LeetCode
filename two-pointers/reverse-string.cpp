632ca54a48310e304d59d27b1f81848b3d7c7c76
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2026-08-16 21:37:25 +0530

    LC-reverse-string [Runtime: N/A; Memory: N/A]

--- two-pointers/reverse-string.cpp
+++ two-pointers/reverse-string.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int l = 0;
+        int i = 0;
         int r = s.size() - 1;
 
         while(l < r) {
-            swap(s[l], s[r]);
+            swap(s[l], s[r])
             l++;
             r--;
         }
