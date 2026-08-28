f18e035e5813b6899cf6101df6ac9c2583709282
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2026-08-28 20:16:15 +0530

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- memoization/climbing-stairs.cpp
+++ memoization/climbing-stairs.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        return n;
+        if (n <= 2) {
+            return n;
+        }
+
+        int a = 1;
+        int b = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return b;
     }
 };
